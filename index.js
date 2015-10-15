f18463b3f25e220199cc28aba52f37844212eead
Matthew Mueller <mattmuelle@gmail.com>
2015-10-14 22:27:47 -0700

    rename internals

--- index.js
+++ index.js
@@ -6,37 +6,37 @@ var compose = require('koa-compose')
 var debug = require('debug')
 
 /**
- * Export `statistics`
+ * Export `hooks`
  */
 
-module.exports = Statistics
+module.exports = Hooks
 
 /**
- * Initialize the statistics
+ * Initialize the hooks
  *
  * @param {Function} marker
  * @param {Function} reduce
  * @return {Function}
  */
 
-function Statistics (marker, reduce) {
+function Hooks (marker, reduce) {
 
-  return function statistics (generator) {
+  return function hooks (generator) {
     var name = generator.name || 'middleware'
     return compose([ tracer, generator ])
 
     function * tracer (next) {
-      var stats = this.state.statistics = this.state.statistics || {}
+      var hooks = this.state.hooks = this.state.hooks || {}
       var start = marker()
 
       // diff with the previous start
-      if (stats.upper) {
-        stats.upper_diff.push([ stats.upper[ stats.upper.length - 1 ], start ])
-        stats.upper.push(start)
+      if (hooks.upper) {
+        hooks.upper_diff.push([ hooks.upper[ hooks.upper.length - 1 ], start ])
+        hooks.upper.push(start)
       } else {
         // start of last composed function, diff with end of last composed function
-        stats.upper = [start]
-        stats.upper_diff = []
+        hooks.upper = [start]
+        hooks.upper_diff = []
       }
 
       // go through the remaining middleware
@@ -45,15 +45,15 @@ function Statistics (marker, reduce) {
       var end = marker()
 
       // diff the previous end
-      if (stats.lower) {
-        stats.lower_diff.push([ stats.lower[stats.lower.length - 1], end ])
-        var beg = stats.upper_diff.pop()
-        var fin = stats.lower_diff.shift()
+      if (hooks.lower) {
+        hooks.lower_diff.push([ hooks.lower[hooks.lower.length - 1], end ])
+        var beg = hooks.upper_diff.pop()
+        var fin = hooks.lower_diff.shift()
         reduce(name, beg[0], beg[1], fin[0], fin[1])
       } else {
         // end of last composed function, diff with start of last composed function
-        stats.lower = [end]
-        stats.lower_diff = []
+        hooks.lower = [end]
+        hooks.lower_diff = []
         reduce(name, start, end)
       }
     }
